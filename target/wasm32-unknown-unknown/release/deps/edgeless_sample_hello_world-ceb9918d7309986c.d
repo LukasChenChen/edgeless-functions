9ff0d5dddc9056324a048cc372471b5072b9f5c0
LukasChenChen <luaks.c.chen@gmail.com>
2024-02-22 16:31:54 +0000

    remove target folder

--- target/wasm32-unknown-unknown/release/deps/edgeless_sample_hello_world-ceb9918d7309986c.d
+++
@@ -1,5 +0,0 @@
-/home/chen/cc/project/edgeless-functions/target/wasm32-unknown-unknown/release/deps/edgeless_sample_hello_world-ceb9918d7309986c.wasm: src/main.rs
-
-/home/chen/cc/project/edgeless-functions/target/wasm32-unknown-unknown/release/deps/edgeless_sample_hello_world-ceb9918d7309986c.d: src/main.rs
-
-src/main.rs:
--- target/wasm32-unknown-unknown/release/edgeless_sample_hello_world.d
+++
@@ -1,1 +0,0 @@
-/home/chen/cc/project/edgeless-functions/target/wasm32-unknown-unknown/release/edgeless_sample_hello_world.wasm: /home/chen/cc/project/edgeless-functions/src/main.rs
