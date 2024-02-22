616fc6fb12b422a435d7b0b013181a9abff5e010
LukasChenChen <luaks.c.chen@gmail.com>
2024-02-22 17:02:06 +0000

    fix for arm

--- target/aarch64-unknown-linux-gnu/debug/deps/edgeless_sample_hello_world-e8b00ac793c17a29.d
+++
@@ -1,5 +0,0 @@
-/home/chen/cc/project/edgeless-functions/target/aarch64-unknown-linux-gnu/debug/deps/edgeless_sample_hello_world-e8b00ac793c17a29: src/main.rs
-
-/home/chen/cc/project/edgeless-functions/target/aarch64-unknown-linux-gnu/debug/deps/edgeless_sample_hello_world-e8b00ac793c17a29.d: src/main.rs
-
-src/main.rs:
