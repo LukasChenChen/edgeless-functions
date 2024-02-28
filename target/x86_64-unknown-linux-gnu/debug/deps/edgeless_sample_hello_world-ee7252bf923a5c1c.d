118ba4f51e94f02be8631a5fc32807ba5c3e6c0d
LukasChenChen <luaks.c.chen@gmail.com>
2024-02-28 13:18:16 +0000

    only add binary

--- target/x86_64-unknown-linux-gnu/debug/deps/edgeless_sample_hello_world-ee7252bf923a5c1c.d
+++
@@ -1,5 +0,0 @@
-/home/chen/cc/project/edgeless-functions/target/x86_64-unknown-linux-gnu/debug/deps/edgeless_sample_hello_world-ee7252bf923a5c1c: src/main.rs
-
-/home/chen/cc/project/edgeless-functions/target/x86_64-unknown-linux-gnu/debug/deps/edgeless_sample_hello_world-ee7252bf923a5c1c.d: src/main.rs
-
-src/main.rs:
--- target/x86_64-unknown-linux-gnu/debug/edgeless_sample_hello_world.d
+++
@@ -1,1 +0,0 @@
-/home/chen/cc/project/edgeless-functions/target/x86_64-unknown-linux-gnu/debug/edgeless_sample_hello_world: /home/chen/cc/project/edgeless-functions/src/main.rs
